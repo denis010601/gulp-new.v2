ca7ff7ee34b5df19e6645687772470a84d4adec5
Denis <deniska01.06.01@gmail.com>
2022-02-11 19:05:44 +0300

    new gulp 2.22

---
+++ config/path.js
@@ -0,0 +1,38 @@
+const pathSrc = "./src";
+const pathDest = "./public";
+
+module.exports = {
+  root: pathDest,
+  html: {
+    src: pathSrc + "/html/*.html",
+    watch: pathSrc + "/html/*.html",
+    dest: pathDest
+  },
+
+  pug: {
+    src: pathSrc + "/*.pug",
+    watch: pathSrc + "/**/*.pug",
+    dest: pathDest
+  },
+
+  css: {
+    src: pathSrc + "/css/*.css",
+    watch: pathSrc + "/css/**/*.css",
+    dest: pathDest + "/css"
+  },
+  js: {
+    src: pathSrc + "/js/*.js",
+    watch: pathSrc + "/js/**/*.js",
+    dest: pathDest + "/js"
+  },
+  scss: {
+    src: pathSrc + "/scss/*.{sass, scss}",
+    watch: pathSrc + "/scss/**/*.{sass, scss}",
+    dest: pathDest + "/css"
+  },
+  img: {
+    src: pathSrc + "/img/**/*.*",
+    watch: pathSrc + "/img/**/*.*",
+    dest: pathDest + "/img"
+  }
+}
